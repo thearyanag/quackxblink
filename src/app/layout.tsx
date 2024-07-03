1ee503d4003da35b24f63fcc56695b7d1f3a0877
aryan <agarwalaryan139@gmail.com>
2024-07-03 12:56:05 +0530

    feat: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,68 +4,56 @@ import "./globals.css";
 import SolanaWalletProvider from "@/providers/SolanaWalletProvider";
 import { Suspense } from "react";
 import BottomNavBar from "./components/BottomNavBar";
-import { APP_TITLE, APP_DESCRIPTION } from "@/app/utils/const"
+import { APP_TITLE, APP_DESCRIPTION } from "@/app/utils/const";
 import LayoutBody from "./components/LayoutBody";
+import { headers } from "next/headers";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: {
-    default: APP_TITLE,
-    template: `%s | ${APP_TITLE}`,
-  },
-  description: APP_DESCRIPTION,
-  applicationName: APP_TITLE,
-  appleWebApp: {
-    capable: true,
-    title: APP_TITLE,
-    statusBarStyle: "default",
-  },
-  formatDetection: {
-    telephone: false,
-  },
-  themeColor: "#FFFFFF",
-  icons: {
-    icon: [
-      { url: "/icons/icon-192x192.png", sizes: "192x192" },
-      { url: "/icons/icon-384x384.png", sizes: "384x384" },
-      { url: "/icons/icon-512x512.png", sizes: "512x512" },
-    ],
-    apple: [
-      { url: "/icons/icon-192x192.png", sizes: "192x192" },
-      { url: "/icons/icon-384x384.png", sizes: "384x384" },
-      { url: "/icons/icon-512x512.png", sizes: "512x512" },
-    ],
-    shortcut: "/favicon.ico",
-  },
-  manifest: "/manifest.json",
-  // twitter: {
-  //   card: "summary",
-  //   title: APP_TITLE,
-  //   description: APP_TITLE,
-  //   creator: "@quack_fun",
-  //   images: [`${}/icons/icon-192x192.png`],
-  // },
-  // openGraph: {
-  //   type: "website",
-  //   title: APP_TITLE,
-  //   description: APP_TITLE,
-  //   siteName: APP_TITLE,
-  //   url: ,
-  //   images: [
-  //     {
-  //       url: `${}/icons/icon-192x192.png`,
-  //       width: 192,
-  //       height: 192,
-  //     },
-  //   ],
-  // },
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    minimumScale: 1,
-    viewportFit: "cover",
-  },
+export async function generateMetadata(): Promise<Metadata> {
+  let title = APP_TITLE;
+  let description = APP_DESCRIPTION;
+
+  title = `${APP_TITLE} | Developers`;
+  description = `Developers | ${APP_DESCRIPTION}`;
+
+  return {
+    title: {
+      default: title,
+      template: `%s | ${APP_TITLE}`,
+    },
+    description,
+    applicationName: APP_TITLE,
+    appleWebApp: {
+      capable: true,
+      title: APP_TITLE,
+      statusBarStyle: "default",
+    },
+    formatDetection: {
+      telephone: false,
+    },
+    themeColor: "#FFFFFF",
+    icons: {
+      icon: [
+        { url: "/icons/icon-192x192.png", sizes: "192x192" },
+        { url: "/icons/icon-384x384.png", sizes: "384x384" },
+        { url: "/icons/icon-512x512.png", sizes: "512x512" },
+      ],
+      apple: [
+        { url: "/icons/icon-192x192.png", sizes: "192x192" },
+        { url: "/icons/icon-384x384.png", sizes: "384x384" },
+        { url: "/icons/icon-512x192.png", sizes: "512x512" },
+      ],
+      shortcut: "/favicon.ico",
+    },
+    manifest: "/manifest.json",
+    viewport: {
+      width: "device-width",
+      initialScale: 1,
+      minimumScale: 1,
+      viewportFit: "cover",
+    },
+  };
 }
 
 export default function RootLayout({
@@ -78,9 +66,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Suspense>
           <SolanaWalletProvider>
-            <LayoutBody>
-            {children}
-            </LayoutBody>
+            <LayoutBody>{children}</LayoutBody>
           </SolanaWalletProvider>
         </Suspense>
       </body>
